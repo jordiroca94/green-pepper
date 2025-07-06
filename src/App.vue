baa175c7cbee6ef24b22978c1dd5b955cd487c33
Jordi Roca <jordi.roca@acaitravel.com>
2025-07-06 23:36:45 +0200

    feat:css clean tailwind setup

--- src/App.vue
+++ src/App.vue
@@ -4,14 +4,19 @@ import { RouterLink, RouterView } from 'vue-router'
 
 <template>
   <header>
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
+    <nav class="bg-white border-b border-gray-200 shadow-sm px-4 sm:px-6 py-3">
+      <div class="max-w-7xl mx-auto flex items-center justify-between">
+        <RouterLink to="/" class="flex items-center space-x-2">
+          <img src="./assets/images/green-pepper.png" alt="Green Pepper Logo" class="w-10 h-10" />
+          <span class="text-xl font-bold text-green-600">Green Pepper</span>
+        </RouterLink>
+        <div class="hidden sm:flex space-x-6">
+          <RouterLink to="/register" class="text-green-600 hover:underline transition"
+            >Register</RouterLink
+          >
+        </div>
+      </div>
+    </nav>
   </header>
 
   <RouterView />
